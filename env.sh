0b040918b2482a39c12ed99e5e62f5c3238060ad
Raoul Hecky <raoul.hecky@gmail.com>
2016-06-30 15:07:46 +0000

    add path for devtool

--- env.sh
+++ env.sh
@@ -2,5 +2,5 @@ BASE=`pwd`
 unset LC_ALL
 export LC_ALL 
 export LANG=en_US.UTF-8 
-export PATH=${BASE}/src/openembedded-core/scripts:${BASE}/src/bitbake/bin:$PATH 
+export PATH=${BASE}/src/openembedded-core/scripts:${BASE}/src/oe-core/scripts:${BASE}/src/bitbake/bin:$PATH 
 export BUILDDIR=${BASE}
